1b377ce272a5955bc1feeff7a4e5a111c84e77c8
swetharth <swetharth@gmail.com>
2024-10-03 21:33:14 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,14 @@ import Todo from './components/Todo';
 import './App.css';
 
 const App = () => {
+
+
+
+  useEffect(() => {
+    fetch("https://todo2-2kl5.onrender.com")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  },[]);
   return (
     <div className="App">
       <Todo />
